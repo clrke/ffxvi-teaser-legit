efb047c05f44cc56cbdef6a65f45540cb066f30d
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2020-10-05 00:46:45 +0800

    Updates

---
+++ precache-manifest.d0d929e51fb5c68b4874ec415097b7c6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b80b143d1f7f1f36c81f9ccc9554cf",
+    "url": "/ffxvi-teaser-legit/index.html"
+  },
+  {
+    "revision": "e9d35bbd52cf261fe0d1",
+    "url": "/ffxvi-teaser-legit/static/css/main.be8d400b.chunk.css"
+  },
+  {
+    "revision": "23fec7dc9f730216dc5f",
+    "url": "/ffxvi-teaser-legit/static/js/2.cd78c0d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ffxvi-teaser-legit/static/js/2.cd78c0d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d35bbd52cf261fe0d1",
+    "url": "/ffxvi-teaser-legit/static/js/main.2619feca.chunk.js"
+  },
+  {
+    "revision": "4c1999922dd031966476",
+    "url": "/ffxvi-teaser-legit/static/js/runtime-main.1881bd71.js"
+  },
+  {
+    "revision": "1a71b9c62073e3a56fa2e301e8b533e3",
+    "url": "/ffxvi-teaser-legit/static/media/chickupo.1a71b9c6.png"
+  },
+  {
+    "revision": "2b72f66bee4920f2ce0733232241733e",
+    "url": "/ffxvi-teaser-legit/static/media/concept-art.2b72f66b.jpg"
+  },
+  {
+    "revision": "ea0835646ea48bb6919138e090ffc5f9",
+    "url": "/ffxvi-teaser-legit/static/media/eikon-moves.ea083564.png"
+  },
+  {
+    "revision": "a7d08ca38d001b1649e57319aa19b257",
+    "url": "/ffxvi-teaser-legit/static/media/eikons.a7d08ca3.png"
+  },
+  {
+    "revision": "5660796af00480d853f24f87d5b85a30",
+    "url": "/ffxvi-teaser-legit/static/media/ffxvi-logo.5660796a.jpeg"
+  },
+  {
+    "revision": "39404554ef86320a62e2bc0500a20297",
+    "url": "/ffxvi-teaser-legit/static/media/ffxvi-trailer.39404554.mp4"
+  },
+  {
+    "revision": "e18dd4a0b104e41de1dcf7323e06decf",
+    "url": "/ffxvi-teaser-legit/static/media/joshua.e18dd4a0.png"
+  },
+  {
+    "revision": "d162b633028a7c05c8b5f074899f52f0",
+    "url": "/ffxvi-teaser-legit/static/media/season-pass-middle-background.d162b633.jpg"
+  },
+  {
+    "revision": "5bcd2829e9bff0b795b4882630ea4cbd",
+    "url": "/ffxvi-teaser-legit/static/media/shield.5bcd2829.png"
+  }
+]);
